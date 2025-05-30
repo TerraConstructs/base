b837c0c3116854a4995152abc76c142059f6ec29
zack hee <zack.hee@handshakes.com.my>
2025-05-30 14:57:33 +0800

    fix: test ported from aws cdk

--- test/aws/edge/load-balancer.test.ts
+++ test/aws/edge/load-balancer.test.ts
@@ -1,6 +1,10 @@
-import { Testing } from "cdktf";
+// https://github.com/aws/aws-cdk/blob/v2.199.0/packages/aws-cdk-lib/aws-route53-targets/test/load-balancer-target.test.ts
+
 import "cdktf/lib/testing/adapters/jest";
-import { edge, AwsStack } from "../../../src/aws";
+import { edge, AwsStack, compute } from "../../../src/aws";
+import { Template } from "../../assertions";
+import { route53Record } from "@cdktf/provider-aws";
+import { Testing } from "cdktf";
 
 const environmentName = "Test";
 const gridUUID = "123e4567-e89b-12d3";
@@ -9,31 +13,80 @@ const gridBackendConfig = {
 };
 const providerConfig = { region: "us-east-1" };
 
-const LB_ZONE_ID = "H6JUI97EXAMPL3";
-const LB_DNS = "alb-1234567890.us-east-1.elb.amazonaws.com";
-
 describe("Load-Balancers", () => {
-  test("Synthesises and matches snapshot", () => {
+  test("use ALB as record target", () => {
     // GIVEN
     const stack = getAwsStack();
-
-    // a public hosted zone
-    const zone = new edge.DnsZone(stack, "Zone", {
-      zoneName: "example.com",
+    const vpc = new compute.Vpc(stack, "VPC", { maxAzs: 2 });
+    const lb = new compute.ApplicationLoadBalancer(stack, "LB", { vpc });
+    const zone = new edge.DnsZone(stack, "HostedZone", {
+      zoneName: "test.public",
     });
 
-    // WHEN – create an A-record that aliases to the existing ALB/NLB
+    // WHEN
+    const record = new edge.ARecord(stack, "LoadBalancerAlias", {
+      zone,
+      recordName: "_foo",
+      target: edge.RecordTarget.fromAlias(new edge.LoadBalancerTarget(lb)),
+    });
+
+    Template.synth(stack).toHaveResourceWithProperties(
+      route53Record.Route53Record,
+      {
+        alias: {
+          evaluate_target_health: true,
+          name: `dualstack.${stack.resolve(lb.loadBalancerDnsName)}`,
+          zone_id: stack.resolve(lb.loadBalancerCanonicalHostedZoneId),
+        },
+      },
+    );
+  });
+
+  //   .hasResourceProperties('AWS::Route53::RecordSet', {
+  //     AliasTarget: {
+  //       DNSName: { 'Fn::Join': ['', ['dualstack.', { 'Fn::GetAtt': ['LB8A12904C', 'DNSName'] }]] },
+  //       HostedZoneId: { 'Fn::GetAtt': ['LB8A12904C', 'CanonicalHostedZoneID'] },
+  //     },
+  //   });
+  // }
+
+  test("use ALB as record target with health check", () => {
+    // GIVEN
+    const stack = getAwsStack();
+    const vpc = new compute.Vpc(stack, "VPC", { maxAzs: 2 });
+    const lb = new compute.ApplicationLoadBalancer(stack, "LB", {
+      vpc,
+      internetFacing: true,
+    });
+    const zone = new edge.DnsZone(stack, "HostedZone", {
+      zoneName: "test.public",
+    });
+
+    // WHEN
     new edge.ARecord(stack, "LoadBalancerAlias", {
       zone,
-      recordName: "alb",
-      target: edge.RecordTarget.fromAlias(
-        edge.LoadBalancerTarget.fromAttributes(LB_ZONE_ID, LB_DNS),
-      ),
+      recordName: "_foo",
+      target: edge.RecordTarget.fromAlias(new edge.LoadBalancerTarget(lb)),
     });
 
-    // THEN – snapshot-test the synthesised Terraform
-    expect(Testing.synth(stack)).toMatchSnapshot();
+    // THEN
+    Template.synth(stack).toHaveResourceWithProperties(
+      route53Record.Route53Record,
+      {
+        alias: {
+          evaluate_target_health: true,
+          name: `dualstack.${stack.resolve(lb.loadBalancerDnsName)}`,
+          zone_id: stack.resolve(lb.loadBalancerCanonicalHostedZoneId),
+        },
+      },
+    );
   });
+
+  // .hasResourceProperties('AWS::Route53::RecordSet', {
+  //   AliasTarget: {
+  //     EvaluateTargetHealth: true,
+  //   },
+  // });
 });
 
 function getAwsStack(): AwsStack {
