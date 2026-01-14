bfcd08fc9a469c775833f18752270e2001341856
Eduardo Rodrigues <16357187+eduardomourar@users.noreply.github.com>
2026-01-14 16:36:20 +0000

    feat(aws/compute): allow import from lookup of vpc

--- src/aws/compute/route.ts
+++ src/aws/compute/route.ts
@@ -1084,6 +1084,7 @@ function routerTypeToPropName(routerType: RouterType) {
     [RouterType.TRANSIT_GATEWAY]: "transitGatewayId",
     [RouterType.VPC_PEERING_CONNECTION]: "vpcPeeringConnectionId",
     [RouterType.VPC_ENDPOINT]: "vpcEndpointId",
+    [RouterType.CORE_NETWORK]: "coreNetworkArn",
   };
   return lookupMap[routerType];
 }
